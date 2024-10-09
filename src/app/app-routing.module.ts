2bd2b6c9225ce0e19c5c6419049c7b59eba996e2
vikas seth <vikas.seth@mullenlowelintas.in>
2024-10-09 11:42:15 +0530

    make routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Component/about/about.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { HomeComponent } from './Component/home/home.component';
+import { ServicesComponent } from './Component/services/services.component';
+import { BackendDevelopmentTechnologiesComponent } from './Technology/backendDevelopmentTechnologies/backendDevelopmentTechnologies.component';
+import { CloudDevelopmentTechnologiesComponent } from './Technology/cloudDevelopmentTechnologies/cloudDevelopmentTechnologies.component';
+import { CMSDevelopmentTechnologiesComponent } from './Technology/CMSDevelopmentTechnologies/CMSDevelopmentTechnologies.component';
+import { DatabaseDevelopmentTechnologiesComponent } from './Technology/databaseDevelopmentTechnologies/databaseDevelopmentTechnologies.component';
+import { DevOpsDevelopmentTechnologiesComponent } from './Technology/devOpsDevelopmentTechnologies/devOpsDevelopmentTechnologies.component';
+import { FrameworksDevevelopmentTechnologiesComponent } from './Technology/frameworksDevevelopmentTechnologies/frameworksDevevelopmentTechnologies.component';
+import { FrontendDevelopmentTechnologiesComponent } from './Technology/frontendDevelopmentTechnologies/frontendDevelopmentTechnologies.component';
+import { MobileDevelopmentTechnologiesComponent } from './Technology/mobileDevelopmentTechnologies/mobileDevelopmentTechnologies.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },         // Home route
+  { path: 'about', component: AboutComponent },   // About route
+  { path: 'service', component: ServicesComponent }, // Services route
+  { path: 'contact', component: ContactComponent }, // Contact route
+  { path: 'frontend-development', component: FrontendDevelopmentTechnologiesComponent },
+  { path: 'backend-development', component: BackendDevelopmentTechnologiesComponent },
+  { path: 'mobile-development', component: MobileDevelopmentTechnologiesComponent },
+  { path: 'database-development', component: DatabaseDevelopmentTechnologiesComponent },
+  { path: 'frameworks-development', component: FrameworksDevevelopmentTechnologiesComponent },
+  { path: 'cloud-development', component: CloudDevelopmentTechnologiesComponent },
+  { path: 'devops-development', component: DevOpsDevelopmentTechnologiesComponent },
+  { path: 'cms-development', component: CMSDevelopmentTechnologiesComponent },
+  { path: '**', redirectTo: '' }      
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { InfoSectionComponent } from './Component/Footer/footer.component';
+import { AboutComponent } from './Component/about/about.component';
+import { ServicesComponent } from './Component/services/services.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { HomeComponent } from './Component/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BackendDevelopmentTechnologiesComponent } from './Technology/backendDevelopmentTechnologies/backendDevelopmentTechnologies.component';
+import { CloudDevelopmentTechnologiesComponent } from './Technology/cloudDevelopmentTechnologies/cloudDevelopmentTechnologies.component';
+import { CMSDevelopmentTechnologiesComponent } from './Technology/CMSDevelopmentTechnologies/CMSDevelopmentTechnologies.component';
+import { DatabaseDevelopmentTechnologiesComponent } from './Technology/databaseDevelopmentTechnologies/databaseDevelopmentTechnologies.component';
+import { DevOpsDevelopmentTechnologiesComponent } from './Technology/devOpsDevelopmentTechnologies/devOpsDevelopmentTechnologies.component';
+import { FrameworksDevevelopmentTechnologiesComponent } from './Technology/frameworksDevevelopmentTechnologies/frameworksDevevelopmentTechnologies.component';
+import { FrontendDevelopmentTechnologiesComponent } from './Technology/frontendDevelopmentTechnologies/frontendDevelopmentTechnologies.component';
+import { MobileDevelopmentTechnologiesComponent } from './Technology/mobileDevelopmentTechnologies/mobileDevelopmentTechnologies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoSectionComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    HomeComponent, 
+    
+    FrontendDevelopmentTechnologiesComponent,
+    BackendDevelopmentTechnologiesComponent,
+    MobileDevelopmentTechnologiesComponent,
+    DatabaseDevelopmentTechnologiesComponent,
+    FrameworksDevevelopmentTechnologiesComponent,
+    CloudDevelopmentTechnologiesComponent,
+    DevOpsDevelopmentTechnologiesComponent,
+    CMSDevelopmentTechnologiesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule ,
+
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
